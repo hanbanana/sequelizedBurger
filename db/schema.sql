284bc6ed63d08e7a5c6f7d75fb4b49a372f8d69d
Han <2han@live.com>
2018-07-30 20:32:03 -0700

    database

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 -- Schema
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
+DROP TABLE IF EXISTS burgers;
 CREATE TABLE burgers
 (
   id INT PRIMARY KEY AUTO_INCREMENT,
--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,30 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+module.exports = function(sequelize, DataTypes) {
+  var Burgers = sequelize.define('burgers', {
+      id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false
+      },
+      burger_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [1],
+              notEmpty: true
+          }
+      },
+      devoured: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: 0
+      },
+      date: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+      }
+  }, {
+      timestamps: false
+  });
 
-var burger = {
-  all: function (cb) {
-    orm.all("burgers", function (res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function (cols, vals, cb) {
-    orm.create("burgers", cols, vals, function (res) {
-      cb(res);
-    });
-  },
-  update: function (objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function (res) {
-      cb(res);
-    });
-  },
-  delete: function (condition, cb) {
-    orm.delete("burgers", condition, function (res) {
-      cb(res);
-    });
-  }
+  return Burgers;
 };
-
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
