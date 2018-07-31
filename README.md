37cd4ee2c08ad1112aeccc5c56ec0c58c0aa0e4e
Han <2han@live.com>
2018-07-30 20:17:07 -0700

    added files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Eat-Da-Burger
+## Instructions
+1. Type a burger name into the app and "Submit" it.
+2. Select a burger you would like to **Devour**
+
+
+![Initial Customer](./screenshots/screenshot.png)
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Seeds
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ham and Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
